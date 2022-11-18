332e1e3bbc5d8a5f214824f17b3f86f0229323d1
Ethan Chee <ethancheez@gmail.com>
2022-11-17 20:12:24 -1000

    cleanup

--- Teensy/agent_ground/lib/config/artemis_defs.cpp
+++ Teensy/agent_ground/lib/config/artemis_defs.cpp
@@ -21,20 +21,15 @@ Threads::Mutex main_queue_mtx;
 Threads::Mutex astrodev_queue_mtx;
 Threads::Mutex rfm23_queue_mtx;
 Threads::Mutex rfm98_queue_mtx;
-Threads::Mutex pdu_queue_mtx;
-Threads::Mutex rpi_queue_mtx;
 
 // Command Queues
 queue<PacketComm> main_queue;
 queue<PacketComm> astrodev_queue;
 queue<PacketComm> rfm23_queue;
 queue<PacketComm> rfm98_queue;
-queue<PacketComm> pdu_queue;
-queue<PacketComm> rpi_queue;
 
 // Other Mutex
 Threads::Mutex spi1_mtx;
-Threads::Mutex i2c1_mtx;
 
 // Utility Functions
 int kill_thread(char *thread_name)
