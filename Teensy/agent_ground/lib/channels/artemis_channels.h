ee9e78214ac63d4714811533bc8b6677423799b0
Ethan Chee <ethancheez@gmail.com>
2022-11-17 20:14:05 -1000

    cleanup includes

--- Teensy/agent_ground/lib/channels/artemis_channels.h
+++ Teensy/agent_ground/lib/channels/artemis_channels.h
@@ -7,12 +7,6 @@
 #include <rfm23.h>
 #include <rfm98.h>
 #include <astrodev.h>
-#include <Wire.h>
-#include <i2c_driver_wire.h>
-#include <Adafruit_LIS3MDL.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_LSM6DSOX.h>
-#include <Adafruit_INA219.h>
 #include <support/packetcomm.h>
 
 namespace Artemis
