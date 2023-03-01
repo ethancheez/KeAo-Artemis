34eaac9e2eb19465ea66c8f1ee6e94aa1a175e1e
Ethan Chee <61472206+ethancheez@users.noreply.github.com>
2023-03-01 10:14:16 -1000

    Add private.h template
    DO NOT COMMIT ACTUAL AES KEY OR IV SIZE

---
+++ Teensy/keao_fsw/include/private.h
@@ -0,0 +1,8 @@
+#ifndef _PRIVATE_H
+#define _PRIVATE_H
+
+/* openssl enc -aes-256-ctr -k secret -P -md sha512 -pbkdf2 */
+#define AES_256_KEY "secretkey"
+#define AES_IV_SIZE 0
+
+#endif // _PRIVATE_H
